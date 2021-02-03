d18a518af055e418175e4e1b4bf9acdfbc5dfed0
Alex Poulos <alex@equul.us>
2021-02-03 06:33:22 -0500

    Initial commit

---
+++ lib/routing/model.ts
@@ -0,0 +1,39 @@
+export interface Route {
+  httpMethod: HTTPMethod;
+  pathPattern: string;
+  handler: RouteHandler;
+}
+
+export interface RouteHandlerInput {
+  pathParameters: Record<string, string>;
+  queryStringParameters: Record<string, string>;
+  body: Record<string, unknown> | string;
+  headers?: Record<string, string>;
+}
+
+export type RouteHandler = (
+  params: RouteHandlerInput
+) => Promise<RouteHandlerResponse>;
+
+export interface RouteHandlerResponse {
+  statusCode: number;
+  body: Record<string, unknown> | string;
+  headers?: Record<string, string>;
+}
+
+export interface LambdaResponse {
+  statusCode: number;
+  body: string;
+  headers?: Record<string, string>;
+}
+
+export type HTTPMethod =
+  | "GET"
+  | "HEAD"
+  | "POST"
+  | "PUT"
+  | "DELETE"
+  | "CONNECT"
+  | "OPTIONS"
+  | "TRACE"
+  | "PATCH";
